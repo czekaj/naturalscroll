0e284cab8f96287908d32bb58e897022c17f9dda
Lucas Czekaj <lukasz@czekaj.us>
2017-10-17 19:44:12 -0700

    add icon and product info

--- naturalscroll/Properties/AssemblyInfo.cs
+++ naturalscroll/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("naturalscroll")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Natural Scroll Enabler")]
+[assembly: AssemblyDescription("Allows mouse to scroll in a Mac way")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("naturalscroll")]
+[assembly: AssemblyProduct("Natural Scroll Enabler")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
